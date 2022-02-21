41d200ae5fbf0d36cfc30db8cfe83e2dcfc161eb
Javier Martinez <javimartinezmma@gmail.com>
2022-02-21 18:03:34 +0100

    update FaceMesh

--- src/faceMesh/FaceMesh.py
+++ src/faceMesh/FaceMesh.py
@@ -6,7 +6,7 @@ class FaceMesh:
     def __init__(self):
         self.mp_face_mesh = mp.solutions.face_mesh
         self.mp_drawing = mp.solutions.drawing_utils
-        self.mp_drawing_styles = mp.solutions.drawing_styles
+        self.drawing_spec = self.mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
 
         self.face_mesh = self.mp_face_mesh.FaceMesh(
             static_image_mode=False,
@@ -23,21 +23,62 @@ class FaceMesh:
         if self.results.multi_face_landmarks:
             for face_landmarks in self.results.multi_face_landmarks:
                 self.mp_drawing.draw_landmarks(
-                    image=image, 
-                    landmark_list=face_landmarks,
-                    connections=self.mp_face_mesh.FACEMESH_TESSELATION,
-                    landmark_drawing_spec=None,
-                    connection_drawing_spec=self.mp_drawing_styles
-                    .get_default_face_mesh_tesselation_style())
-
-                self.mp_drawing.draw_landmarks(
                     image=image,
                     landmark_list=face_landmarks,
                     connections=self.mp_face_mesh.FACEMESH_CONTOURS,
-                    landmark_drawing_spec=None,
-                    connection_drawing_spec=self.mp_drawing_styles
-                    .get_default_face_mesh_contours_style())
+                    landmark_drawing_spec=self.drawing_spec,
+                    connection_drawing_spec=self.drawing_spec)
         return image
 
     def get_results(self):
         return self.results
+
+""" class FaceMesh:
+    def __init__(self):
+        self.mp_face_mesh = mp.solutions.face_mesh
+        self.mp_drawing = mp.solutions.drawing_utils
+        self.drawing_spec = self.mp_drawing.DrawingSpec(thickness=1, circle_radius=1)
+
+        self.face_mesh = self.mp_face_mesh.FaceMesh(
+            static_image_mode=False,
+            max_num_faces=1,
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5)
+        self.results = None
+
+        self.frame = None
+        self.stopped = False
+        self.processing = False
+
+    def start(self):
+        Thread(target=self.update, args=()).start()
+        return self
+
+    def update(self):
+        while not self.stopped:
+            if self.processing:
+                imageToProcess = cv2.cvtColor(self.frame, cv2.COLOR_BGR2RGB)
+                self.results = self.face_mesh.process(imageToProcess)
+                self.processing = False
+
+    def process(self, image):
+        self.frame = image
+        self.processing = True
+
+    def draw(self, image):
+        if self.results:
+            if self.results.multi_face_landmarks:
+                for face_landmarks in self.results.multi_face_landmarks:
+                    self.mp_drawing.draw_landmarks(
+                        image=image,
+                        landmark_list=face_landmarks,
+                        connections=self.mp_face_mesh.FACEMESH_CONTOURS,
+                        landmark_drawing_spec=self.drawing_spec,
+                        connection_drawing_spec=self.drawing_spec)
+        return image
+
+    def get_results(self):
+        return self.results
+
+    def stop(self):
+        self.stopped = True """

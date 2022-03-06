b32993dce211fb88ea62326165dd580be6670d34
jmrtzma <javimartinezmma@gmail.com>
2022-03-06 19:28:47 +0100

    adding script to train

--- src/expressionRecognition/train.py
+++ src/expressionRecognition/train.py
@@ -9,10 +9,9 @@ from sklearn.metrics import classification_report, confusion_matrix
 
 # Data
 data = pd.read_csv('dataset/dataset.csv')
-print(data)
 
 # Suffle data and division in train and test
-""" X = data.drop(columns = 'y')
+X = data.drop(columns = 'y')
 y = data['y'].astype(int)
 X_train, X_test, y_train, y_test = train_test_split(
                                       X,
@@ -29,4 +28,4 @@ svclassifier.fit(X_train, y_train.ravel())
 # Predictions and model evaluation
 y_pred = svclassifier.predict(X_test)
 print(confusion_matrix(y_test, y_pred))
-print(classification_report(y_test, y_pred)) """
+print(classification_report(y_test, y_pred))

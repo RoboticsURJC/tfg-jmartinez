d808f95ac05111be0d357564e3377f3441714593
Javier Martinez <javimartinezmma@gmail.com>
2022-04-03 21:06:14 +0200

    adding main program

--- src/expressionRecognition/main.py
+++ src/expressionRecognition/main.py
@@ -1,33 +1,127 @@
 import sys
 sys.path.append('..')
-from emotionalMesh.EmotionalMesh2 import EmotionalMesh2
+from emotionalMesh.EmotionalMesh2_1 import EmotionalMesh2_1
+from piVideoStream.PiVideoStream import PiVideoStream
 import cv2
-import glob as gb
-import joblib
+import pickle
+import time
+import argparse
 
-stream = cv2.VideoCapture(1)
-emotionalmesh = EmotionalMesh2(static=False, max_num_faces=1)
+# Variables to calculate and show FPS
+counter, fps = 0, 0
+fps_avg_frame_count = 10
+text_location = (20, 24)
+text_color = (0, 0, 255)  # red
+font_size = 1
+font_thickness = 1
+start_time = time.time()
 
-# Load the model
-model = joblib.load('model/model.pkl')
+# Init time of program
+init_time = time.time()
 
-while(True):
-    grabbed, image = stream.read()
-    if not grabbed:
-        break
+def draw_fps(image, fps):
+    fps_text = 'FPS = {:.1f}'.format(fps)
+    cv2.putText(image, fps_text, text_location, cv2.FONT_HERSHEY_PLAIN,
+                font_size, text_color, font_thickness)
+    return image
 
-    image = cv2.flip(image, 1)
+def calculate_fps():
+    global counter, fps, start_time
+    counter += 1
+    if counter % fps_avg_frame_count == 0:
+        end_time = time.time()
+        fps = fps_avg_frame_count / (end_time - start_time)
+        start_time = time.time()
+    return fps
 
-    emotionalmesh.set_image(image)
-    emotionalmesh.process()
-    if emotionalmesh.face_mesh_detected():
-        angles = emotionalmesh.get_angles()
-        print(model.predict(angles))
+def savefps(fps, max_time, fps_file):
+    fps_file.write('{:.1f}, {:.1f}\n'.format(fps, (time.time() - init_time)))
+    if (time.time() - init_time) >= max_time:
+        print("FPS save is over")
+        return False
+    return True
 
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--savefps',
+                    action='store_true',
+                    default=False, required=False,
+                    help='Activa el guardado de fps en fichero .csv')
+    parser.add_argument('--time',
+                    type=int,
+                    default=30, required=False,
+                    help='Duracion en segundos del guardado de datos (int). Default: 30s')
+    return parser.parse_args()
 
-    # Show image
-    cv2.imshow("imagen", image)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-  
-cv2.destroyAllWindows()
+def draw_predict(pred, image):
+    if pred == 1:
+        emotion = "Anger"
+    elif pred == 2:
+        emotion = "Contempt"
+    elif pred == 3:
+        emotion = "Disgust"
+    elif pred == 4:
+        emotion = "Fear"
+    elif pred == 5:
+        emotion = "Happy"
+    elif pred == 6:
+        emotion = "Sadness"
+    else:
+        emotion = "Surprise"
+    cv2.putText(image, emotion, (20, 456), cv2.FONT_HERSHEY_PLAIN,
+                2, text_color, font_thickness)
+
+def main():
+    args = parse_arguments()
+    if args.savefps:
+        fps_file = open('dataFPS/emotionalMesh/fps_3.csv', 'w')
+    
+    # Start video stream
+    vs = PiVideoStream(resolution=(640, 480)).start()
+    time.sleep(2.0)
+
+    # Init EmotionalMesh
+    emotionalmesh = EmotionalMesh2_1(static=False, max_num_faces=1)
+
+    # Load the model
+    with open('model/model.pkl', 'rb') as modelfile:
+        loaded = pickle.load(modelfile)
+    model = loaded['model']
+    pca = loaded['pca_fit']
+
+    while(True):
+        # Read image from stream
+        image = vs.read()
+        image = cv2.flip(image, 1)
+
+        # Process image
+        emotionalmesh.set_image(image)
+        emotionalmesh.process()
+        emotionalmesh.draw_emotional_mesh()
+        image = emotionalmesh.get_image()
+        if emotionalmesh.face_mesh_detected():
+            angles = emotionalmesh.get_angles()
+            angles = pca.transform(angles)
+            pred = model.predict(angles)
+            draw_predict(pred, image)
+
+        # Calculate the FPS
+        fps = calculate_fps()
+
+        # Save FPS
+        if args.savefps:
+            args.savefps = savefps(fps, args.time, fps_file)
+
+        # Draw the FPS
+        image = draw_fps(image, fps)
+
+        # Show image
+        cv2.imshow("imagen", image)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    
+    cv2.destroyAllWindows()
+    vs.stop()
+
+if __name__ == '__main__':
+    main()
